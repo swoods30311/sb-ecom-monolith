0af6d396edd20663dccf780b02a27bf8d6e55891
swoods30311 <swoods30311@gmail.com>
2026-03-01 18:18:39 -0500

    Set up Category Service code

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,10 +14,17 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return "Category added successfully";
     }
 }
